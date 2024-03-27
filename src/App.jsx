af10f25c5e0f649a341f63ae909a4522768e9a1c
Ifeoma-Nwh <ifeoma.nwaoha10@gmail.com>
2024-03-27 23:02:22 +0100

    added scope to animation

--- src/App.jsx
+++ src/App.jsx
@@ -16,45 +16,48 @@ import { useGSAP } from "@gsap/react";
 gsap.registerPlugin(useGSAP);
 
 function App() {
-  const navContainer = useRef();
+  const appContainer = useRef();
   const tl = useRef();
 
-  useGSAP(() => {
-    gsap.set(navContainer.current, { y: -100 });
-    gsap.set(".letter-wrapper", { y: 400 });
-    gsap.set(".item-copy-wrapper p", { y: 50 });
-    gsap.defaults({ duration: 1, ease: "power3.out" });
+  useGSAP(
+    () => {
+      gsap.set("nav", { y: -100 });
+      gsap.set(".letter-wrapper", { y: 400 });
+      gsap.set(".item-copy-wrapper p", { y: 50 });
+      gsap.defaults({ duration: 1, ease: "power3.out" });
 
-    tl.current = gsap.timeline({ pause: true, delay: 0.5 });
+      tl.current = gsap.timeline({ pause: true, delay: 0.5 });
 
-    tl.current
-      .to(".letter-wrapper", { y: 0, stagger: 0.1 })
-      .to(".header-item-1", { left: "12vw" })
-      .to(".header-item-2", { right: "8vw" }, "<")
-      .to(
-        ".item-main .item-img img",
-        { clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)" },
-        "<"
-      )
-      .to(".header-item-1", { left: 0, scale: 1 })
-      .to(".header-item-2", { right: 0, scale: 1 }, "<")
-      .to(".item-main .item-img img", { scale: 1 }, "<")
-      .to(
-        ".item-side .item-img",
-        {
-          clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%",
-          stagger: 0.1,
-        },
-        "<"
-      )
-      .to(".header", { bottom: "-2rem" }, "<")
-      .to(".item-copy-wrapper p", { y: 0, stagger: 0.05 }, "<")
-      .to("nav", { y: 0 }, "<");
-  });
+      tl.current
+        .to(".letter-wrapper", { y: 0, stagger: 0.1 })
+        .to(".header-item-1", { left: "12vw" })
+        .to(".header-item-2", { right: "8vw" }, "<")
+        .to(
+          ".item-main .item-img img",
+          { clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)" },
+          "<"
+        )
+        .to(".header-item-1", { left: 0, scale: 1 })
+        .to(".header-item-2", { right: 0, scale: 1 }, "<")
+        .to(".item-main .item-img img", { scale: 1 }, "<")
+        .to(
+          ".item-side .item-img",
+          {
+            clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%",
+            stagger: 0.1,
+          },
+          "<"
+        )
+        .to(".header", { bottom: "-2rem" }, "<")
+        .to(".item-copy-wrapper p", { y: 0, stagger: 0.05 }, "<")
+        .to("nav", { y: 0 }, "<");
+    },
+    { scope: appContainer }
+  );
 
   return (
-    <>
-      <nav ref={navContainer}>
+    <div ref={appContainer}>
+      <nav>
         <div className="logo">
           <a href="#">LGNT Vase</a>
         </div>
@@ -201,7 +204,7 @@ function App() {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
